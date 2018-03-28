309ea796c1b66c8dfaa2732759dcb6afe1e33285
Mihovska <vili_mihovska@live.com>
2018-03-28 11:57:09 +0300

    login step definitions modified

--- features/step_definitions/login-steps.js
+++ features/step_definitions/login-steps.js
@@ -7,17 +7,18 @@ var {expect} = require('chai');
 defineSupportCode(function({When, Then, And}) {
 
   When(/^I fill in e-mail with "([^"]*)"$/, function (userEmail, next) {
-    this.driver.get('https://www.google.com/calendar');
-    this.driver.findElement(By.name('identifier'))
+    this.driver.get('https://calendar.google.com/calendar');
+    this.driver.findElement(By.id('identifierId'))
       .sendKeys(userEmail);
-    this.driver.findElement(By.name('identifier'))
+    this.driver.findElement(By.id('identifierId'))
       .sendKeys(Key.ENTER)
         .then(function() {
           next();
         });
   });
 
-  And(/^I fill in Password with "([^"]*)"$/, function (userPass, next) {
+  When(/^I fill in Password with "([^"]*)"$/, function (userPass, next) {
+    this.driver.wait(until.elementLocated(By.name('password')));
     this.driver.findElement(By.name('password'))
       .sendKeys(userPass);
     this.driver.findElement(By.name('password'))
@@ -29,10 +30,11 @@ defineSupportCode(function({When, Then, And}) {
 
 
   Then(/^I should be redirected to my profile page$/, function (next) {
-    this.driver.wait(until.elementLocated(By.css('div.g')));
-    this.driver.findElements(By.css('div.g'))
-      .then(function(elements) {
-        expect(elements.length).to.not.equal(0);
+    this.driver.wait(until.elementLocated(By.css('a.gb_b.gb_ib.gb_R')));
+    var usernameField = this.driver.findElement(By.css('a.gb_b.gb_ib.gb_R'));
+    usernameField.getAttribute('aria-label')
+      .then(function(element) {
+        expect(element).to.include('Test Test');
         next();
       });
   });
