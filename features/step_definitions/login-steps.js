3cf81235202a34c5d877321602d258d677d282d3
Mihovska <vili_mihovska@live.com>
2018-03-29 11:41:23 +0300

    try new steps

--- features/step_definitions/login-steps.js
+++ features/step_definitions/login-steps.js
@@ -30,7 +30,6 @@ defineSupportCode(function({When, Then}) {
         });
   });
 
-
   Then(/^I should be redirected to my profile page$/, function (next) {
     this.driver.wait(until.elementLocated(By.css('a.gb_b.gb_ib.gb_R')));
     var usernameField = this.driver.findElement(By.css('a.gb_b.gb_ib.gb_R'));
@@ -40,6 +39,7 @@ defineSupportCode(function({When, Then}) {
         next();
       });
   });
+
 
   Then(/^I should see error message$/, function (next) {
     this.driver.wait(until.elementLocated(By.css('div.dEOOab.RxsGPe')));
